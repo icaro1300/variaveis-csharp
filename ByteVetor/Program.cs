708befc15bd6b08015dbb58fc15e836e388bba85
sophia <sophai@aluno.senai.br>
2022-10-26 20:50:39 -0300

    27out

---
+++ ByteVetor/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace ByteVetor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] vbytes = { 2, 4, 8, 10 };
+
+            Console.WriteLine("Lista de Valores do Vetor");
+            Console.WriteLine("Valor1: {0}", vbytes[0]);
+            Console.WriteLine("Valor2: {0}", vbytes[1]);
+            Console.WriteLine("Valor3: {0}", vbytes[2]);
+            Console.WriteLine("Valor4: {0}", vbytes[3]);
+            Console.WriteLine("Total de elementos no vetor é: {0}", vbytes.Length);
+
+            for (int i = 0; i < vbytes.Length; i++)
+            {
+                Console.WriteLine("Valor a posição {0} é igual a {1}", i, vbytes[i]);
+
+            }
+            byte[] vbytesB = new byte[3];
+            Console.WriteLine("Novos valores");
+            for (int i = 0; i < vbytesB.Length; i++)
+            {
+                Console.WriteLine("Digite o {0} valor: ", i);
+                try
+                {
+                    vbytesB[i] = Convert.ToByte(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    vbytes[i] = 0;
+                }
+                int contValidos = 0;
+                int contInvalidos = 0;
+                for (int i = 0; i < vbytes.Length; i++)
+                {
+                    if (vbytesB[i] == 0) { contInvalidos ++; }
+                    else { contValidos ++; }
+                    Console.WriteLine("Quantidade de válidos é: {0}", contValidos);
+                    Console.WriteLine("Quantidade de invalidos é: {0}", contInvalidos);
+
+
+
+
+                }
+
+            }
+        }
+    } }
---
+++ TypeByte/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TypeByte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                byte x = Convert.ToByte(Console.ReadLine());
+                Console.WriteLine("O valor digital é: {0}", x);
+            }
+            catch (OverflowException)
+            {
+                Console.WriteLine("O valor digitado está fora do máximo. O máximo é 255");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Você digitou um texto. Só é aceitável um número de 0 a 255");
+            }
+         
+        }
+    }
+}
